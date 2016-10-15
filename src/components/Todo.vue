57b145a44b6074f33bcb6a95ba92c8a92329c190
Joseph Luck <joseph.luck@sky.com>
2016-10-15 12:21:02 +0100

    Adding a simple todo list

---
+++ src/components/Todo.vue
@@ -0,0 +1,42 @@
+<template>
+  <li
+    class="todo"
+    :class="{
+      completed: todo.completed
+    }"
+  >
+    <input
+      class="toggle"
+      type="checkbox"
+      v-on:click="toggleTodo"
+    />
+    <span
+      class="todo-description"
+    >
+      {{todo.text}}
+    </span>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: [
+      'todo'
+    ],
+    methods: {
+      toggleTodo: function () {
+        this.$emit('toggle', this.todo)
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .todo {
+    &.completed {
+      .todo-description {
+        text-decoration: line-through;
+      }
+    }
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="todos">
+    <form
+      @submit="addNewTodo"
+    >
+      <input
+        placeholder="Enter a todo"
+        v-model="addNewTodoText"
+      />
+      <button
+        type="submit"
+      >
+        Save
+      </button>
+    </form>
+    <ol>
+      <template v-for="todo in todos">
+        <todo
+          :todo="todo"
+          v-on:toggle="toggleTodo(todo)"
+        />
+      </template>
+    </ol>
+  </div>
+</template>
+
+<script>
+  import Todo from './Todo.vue'
+  export default {
+    data () {
+      return {
+        addNewTodoText: '',
+        todos: []
+      }
+    },
+    methods: {
+      addNewTodo: function (e) {
+        e.preventDefault()
+        this.todos.push({
+          text: this.addNewTodoText.trim(),
+          completed: false
+        })
+        this.addNewTodoText = ''
+      },
+      toggleTodo: function (todo) {
+        todo.completed = !todo.completed
+      }
+    },
+    components: {
+      Todo
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  h1 {
+    color: #42b983;
+  }
+</style>
